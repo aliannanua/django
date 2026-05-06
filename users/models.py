b702cf56b9d9fdbfc162282a8134324a8573db69
aliannanua <a1402sasha@gmail.com>
2026-05-06 17:39:54 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,19 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
-
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, full_name='', **extra_fields):
         if not email:
             raise ValueError('Email обязателен')
-
         email = self.normalize_email(email)
         user = self.model(email=email, full_name=full_name, **extra_fields)
         user.set_password(password)
         user.save()
         return user
-
-
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=255)
@@ -21,5 +16,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     role = models.CharField(max_length=20, default='user')
 
     objects = UserManager()
-
     USERNAME_FIELD = 'email'

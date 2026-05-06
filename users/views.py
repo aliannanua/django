0afaeec801a4f3b7f90606efbb7a9b284e3efc59
aliannanua <a1402sasha@gmail.com>
2026-05-06 17:41:21 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer
 
-
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
